2d33631b59ba26cf0aa0205880a05be795143a08
Mathias Rentsch <rentsch@online.de>
2014-04-24 20:18:06 +0200

    Final commit with note on license GPL
    - insert reference GPL
    - insert short description in readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿RandomQueue
+===========
+
+Die Klasse RandomQueue generiert nicht negative Zufallszahlen vom Typ System.Int32. Die öffentliche Methode Next() liefert eine Zufallszahl, deren Wert im Bereich von 0 bis zur exklusiven oberen Grenze MaxValue liegt. Die Klasse stellt durch eine Queue sicher, dass nur Zahlen geliefert werden, die sich nicht eine bestimmte Anzahl Aufrufe der Methode Next() wiederholen. Diese Anzahl wird im Kontruktor-Parameter lastIgnoreCount übergeben.
+
--- randomqueue.cs
+++ randomqueue.cs
@@ -1,58 +1,66 @@
-﻿using System;
+﻿/* RandomQueue
+ * --------------------------------------------
+ * Datei:     randomqueue.cs
+ * Version:   24.04.2014
+ * Besitzer:  Mathias Rentsch (rentsch@online.de)
+ * Lizenz:    GPL
+ *
+ * Die Anwendung und die Quelltextdateien sind freie Software und stehen unter der
+ * GNU General Public License. Der Originaltext dieser Lizenz kann eingesehen werden
+ * unter http://www.gnu.org/licenses/gpl.html.
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace MyRadio
+class RandomQueue
 {
-    class RandomQueue
+    private Random random;
+    public Queue<int> queue;
+
+    public RandomQueue(int maxValue, int lastIgnoreCount)
     {
-        private Random random;
-        public Queue<int> queue;
+        random = new Random(DateTime.Now.Millisecond);
+        queue = new Queue<int>();
+        if (maxValue < 3) throw new ArgumentOutOfRangeException("maxValue", "Der Konstruktorparameter maxValue ist kleiner 3 (da macht die Klasse keinen Sinn).");
+        maxvalue = maxValue;
+        if (lastIgnoreCount < 1) throw new ArgumentOutOfRangeException("lastIgnoreCount", "Der Konstruktorparameter lastIgnoreCount ist kleiner 1 (da macht die Klasse keinen Sinn).");
+        if (lastIgnoreCount > maxvalue - 2) throw new ArgumentOutOfRangeException("lastIgnoreCount", "Der Konstruktorparameter lastIgnoreCount ist größer als maxValue kleiner 1 (da macht die Klasse keinen Sinn).");
+        lastignorecount = lastIgnoreCount;
+    }
 
-        public RandomQueue(int maxValue, int lastIgnoreCount)
+    private int maxvalue;
+    public int MaxValue
+    {
+        get
         {
-            random = new Random(DateTime.Now.Millisecond);
-            queue = new Queue<int>();
-            if (maxValue < 3) throw new ArgumentOutOfRangeException("maxValue", "Der Konstruktorparameter maxValue ist kleiner 3 (da macht die Klasse keinen Sinn).");
-            maxvalue = maxValue;
-            if (lastIgnoreCount < 1) throw new ArgumentOutOfRangeException("lastIgnoreCount", "Der Konstruktorparameter lastIgnoreCount ist kleiner 1 (da macht die Klasse keinen Sinn).");
-            if (lastIgnoreCount > maxvalue - 2) throw new ArgumentOutOfRangeException("lastIgnoreCount", "Der Konstruktorparameter lastIgnoreCount ist größer als maxValue kleiner 1 (da macht die Klasse keinen Sinn).");
-            lastignorecount = lastIgnoreCount;
+            return maxvalue;
         }
+    }
 
-        private int maxvalue;
-        public int MaxValue
+    private int lastignorecount;
+    public int LastIngnoreCount
+    {
+        get
         {
-            get
-            {
-                return maxvalue;
-            }
+            return lastignorecount;
         }
+    }
 
-        private int lastignorecount;
-        public int LastIngnoreCount
+    public int Next()
+    {
+        int newRandom;
+        do
         {
-            get
-            {
-                return lastignorecount;
-            }
+            newRandom = random.Next(maxvalue);
         }
+        while (queue.Contains(newRandom));
 
-        public int Next()
-        {
-            int newRandom;
-            do
-            {
-                newRandom = random.Next(maxvalue);
-            } 
-            while (queue.Contains(newRandom));
-            
-            queue.Enqueue(newRandom);
+        queue.Enqueue(newRandom);
 
-            if (queue.Count > lastignorecount) queue.Dequeue();
+        if (queue.Count > lastignorecount) queue.Dequeue();
 
-            return newRandom;
-        }
-
+        return newRandom;
     }
 }
